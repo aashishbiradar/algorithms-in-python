0775d9c0fd1834564dad32bf36cf200a4a0a5d05
aashishbiradar <aashish.biradar@gmail.com>
2019-12-11 06:04:31 +0530

    selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -1,1 +1,12 @@
-print("Hello algos!")
+def selection_sort(list):
+    print(list)
+    n = len(list)
+    for i in range(n):
+        for j in range(i+1,n):
+            if list[i] > list [j]:
+                list[i],list[j] = list[j],list[i]
+    print(list)
+
+selection_sort([2,1,0,5,6])
+
+    
